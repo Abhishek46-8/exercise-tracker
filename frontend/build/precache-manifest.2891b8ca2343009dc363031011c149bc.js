90c80d346df4544f1aa952268b8f586bb0dc5c2a
Abhishek46-8 <79709311+Abhishek46-8@users.noreply.github.com>
2021-11-02 22:01:11 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.2891b8ca2343009dc363031011c149bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efb37da32749ee878e7255e7069af67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "228dfa8799f8114daabf",
+    "url": "/static/css/2.7c0adb80.chunk.css"
+  },
+  {
+    "revision": "228dfa8799f8114daabf",
+    "url": "/static/js/2.d48acbb0.chunk.js"
+  },
+  {
+    "revision": "6882cc347d421bd2892d",
+    "url": "/static/js/main.d8b4fba4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
